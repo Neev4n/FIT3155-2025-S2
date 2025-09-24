c3022640539e9e064fd7078350886a141a175caf
nkan0025 <nkan0025@student.monash.edu>
2025-09-24 15:24:07 +1000

    just comments

--- Assignment1/q2/a1q2test.py
+++ Assignment1/q2/a1q2test.py
@@ -1,7 +1,24 @@
+ASCII_START = 37
+ASCII_END = 126
+ENDING_CHAR = "!"
+def z_algorithm(input_text: str):
+    """
+        Function description: Creates Z array to help with matched suffix and good prefix tables
 
-def z_algorithm(text: str):
+        Input:
+            input_text : input text to compute Z array
 
-    n = len(text)
+        Time complexity: O(S) where S is the length of the string
+
+        Time complexity analysis : Given S is the length of the string,
+
+            creating Z array is O(S)
+            populating Z array is O(S)
+
+            O(S)
+    """
+
+    n = len(input_text)
     l = 0
     r = 0
     Z = [0] * n
@@ -10,7 +27,7 @@ def z_algorithm(text: str):
         #out of box
         if k > r:
             l = r = k
-            while (r < n and text[r-l] == text[r]):
+            while (r < n and input_text[r-l] == input_text[r]):
                 r+= 1
 
             Z[k] = r - l
@@ -24,7 +41,7 @@ def z_algorithm(text: str):
                 Z[k] = Z[k1]
             else:
                 r += 1
-                while (r < n and text[r-l-k1] == text[r]):
+                while (r < n and input_text[r-l-k1] == input_text[r]):
                     r += 1
 
                 r -= 1
@@ -34,32 +51,76 @@ def z_algorithm(text: str):
     return Z
 
 def create_good_prefix_table(pattern: str) -> list[int]:
+    """
+        Function description: Creates good prefix table for good prefix shifts
 
-    Z = z_algorithm(pattern)
+        Input:
+            pattern : input pattern to create table from
 
-    good_prefix_table = [0] * (len(pattern) + 1)
+        Time complexity: O(S) where S is the length of the string
 
+        Time complexity analysis : Given S is the length of the string,
+
+            populating Z array is O(S)
+            creating good prefix table is O(S)
+            populating good prefix table is O(S)
+
+            O(S)
+    """
+
+    # get Z values
+    Z = z_algorithm(pattern)    # O(S) where S is the length of the string
+
+    good_prefix_table = [0] * (len(pattern) + 1)    # O(S)
+
+    # O(S)
+    # If a prefix reoccurs then store the index where it occurs at the end of the prefix in the pattern
+    # scan from right to left
     for i in range(len(pattern)-1, -1, -1):
-        j =  Z[i] - 1
-        good_prefix_table[j] = i
+        j = Z[i] - 1    # end of prefix in the pattern
+        good_prefix_table[j] = i    # store index of next occurrence of prefix at the end of prefix in the pattern
 
     return good_prefix_table
 
 def create_matched_suffix_table(pattern: str)-> list[int]:
+    """
+        Function description: Creates a matched suffix table for matched suffix shifts
 
+        Input:
+            pattern : input pattern to create table from
+
+        Time complexity: O(S) where S is the length of the string
+
+        Time complexity analysis : Given S is the length of the string,
+
+            reversing pattern O(S)
+            creating Z array is O(S)
+            reversing Z array is O(S)
+            creating matched suffix table is O(S)
+            populating matched suffix table is O(S)
+
+            O(S)
+    """
     if not len(pattern):
         return []
 
     m = len(pattern)
 
-    Z = z_algorithm(pattern[::-1])
-    Z.reverse()
-    table = [0] * m
+
+    Z = z_algorithm(pattern[::-1])  #O(S) where S is the length of the string
+    Z.reverse() # O(S)
+    table = [0] * m # O(S)
     table[0] = Z[0]
 
     for i in range(1,m):
-        if Z[i]  == i + 1:
+
+        # if length of the suffix of the reversed string equals the length of the string at a given i
+        # then the prefix must be identical to the suffix at the given i
+        # update the matched prefix value of table[i]
+        if Z[i] == i + 1:
             table[i] = Z[i]
+
+        # else use previous value from i-1
         else:
             table[i] = table[i-1]
 
@@ -67,13 +128,31 @@ def create_matched_suffix_table(pattern: str)-> list[int]:
     return table
 
 def create_bad_character_table(pattern: str) -> list[list[int]]:
+    """
+        Function description: Creates a bad character table to calculate bad character shifts
 
+        Input:
+            pattern : input pattern to create table from
+
+        Time complexity: O(S) where S is the length of the string
+
+        Time complexity analysis : Given S is the length of the string,
+
+            reversing pattern O(S)
+            creating Z array is O(S)
+            reversing Z array is O(S)
+            creating matched suffix table is O(S)
+            populating matched suffix table is O(S)
+
+            O(S)
+    """
+    assci_range = (ASCII_END - ASCII_START)
     p = len(pattern)
-    arr = [[-1] * 26] * (p+1)
-    temp = [-1] * 26
+    arr = [[-1] * assci_range] * (p+1)
+    temp = [-1] * assci_range
 
     for i in range(len(pattern)-1,-1,-1):
-        temp[ord(pattern[i]) - ord('a')] = i
+        temp[ord(pattern[i]) - ASCII_START] = i
         arr[i] = temp[:]
 
     return arr
@@ -113,7 +192,7 @@ def boyer_moore_algorithm(text: str, pattern: str):
             k = k - good_shift
             continue
 
-        bad_char_val = bad_char_table[k1][ord(text[k+k1]) - ord('a')]
+        bad_char_val = bad_char_table[k1][ord(text[k+k1]) - ASCII_START]
         bad_char_shift = p if bad_char_val == -1 else max(1, bad_char_val - k1)
 
         good_prefix_val = good_prefix_table[k1-1]
